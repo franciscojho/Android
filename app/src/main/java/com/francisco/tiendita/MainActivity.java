af8c4eebf28eb3562a212ecf21e5f7bc85c45cd4
Francisco Huapaya <franciscojho@hotmail.com>
2019-08-26 22:36:35 -0500

    Tiendita

---
+++ app/src/main/java/com/francisco/tiendita/MainActivity.java
@@ -0,0 +1,16 @@
+package com.francisco.tiendita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "HOLA JEFFREY", Toast.LENGTH_SHORT).show();
+    }
+}
