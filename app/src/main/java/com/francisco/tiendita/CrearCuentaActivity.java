7cbb3dc6e85c03beadeba7e6563143837f4a9a86
Francisco <franciscojho@hotmail.com>
2019-09-02 21:47:23 -0500

    Tiendita v1.5

---
+++ app/src/main/java/com/francisco/tiendita/CrearCuentaActivity.java
@@ -0,0 +1,14 @@
+package com.francisco.tiendita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+}
